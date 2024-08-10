98dbe4b9e4db60043072fcd47590c9b93c66a359
Lucas Martins <lucasmartins1709@hotmail.com>
2024-08-10 18:43:30 -0300

    refactor: reorganizando o test put

--- tests/test_users/put/test_put.py
+++ tests/test_users/put/test_put.py
@@ -2,6 +2,70 @@ import pytest
 from fastapi import status
 
 from smartkitchien_api.messages.error import ErrorMessages
+from smartkitchien_api.messages.success import SuccessMessages
+from smartkitchien_api.models.user import User
+from smartkitchien_api.security.security import verify_password
+
+
+@pytest.mark.asyncio()
+async def test_update_username_using_just_username(client, faker_user, token):
+    update_faker_user = {'username': 'new_usertest'}
+    headers = {'Authorization': f'Bearer {token}'}
+
+    response = client.put(
+        f'/api/users/{faker_user.id}',
+        json=update_faker_user,
+        headers=headers,
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert 'detail' in response.json()
+    assert response.json()['detail'] == SuccessMessages.UPDATE_USER  # type: ignore
+
+
+@pytest.mark.asyncio()
+async def test_update_email_using_just_email(client, faker_user, token):
+    update_faker_user = {'email': 'new_usertest@example.com'}
+    headers = {'Authorization': f'Bearer {token}'}
+
+    response = client.put(
+        f'/api/users/{faker_user.id}',
+        json=update_faker_user,
+        headers=headers,
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert 'detail' in response.json()
+    assert response.json()['detail'] == SuccessMessages.UPDATE_USER  # type: ignore
+
+
+@pytest.mark.asyncio()
+async def test_update_password_using_just_password(client, faker_user, token):
+    update_faker_user = {'password': 'newmyS&cret007'}
+
+    headers = {'Authorization': f'Bearer {token}'}
+
+    response = client.put(
+        f'/api/users/{faker_user.id}',
+        json=update_faker_user,
+        headers=headers,
+    )
+
+    # Buscar o usuário no banco de dados após a atualização
+    updated_user = await User.get(faker_user.id)
+
+    passwords_match = verify_password(
+        plain_password=update_faker_user['password'],
+        hashed_password=updated_user.password,
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert 'detail' in response.json()
+    assert response.json()['detail'] == SuccessMessages.UPDATE_USER  # type: ignore
+    assert passwords_match
+
+
+# @ ============================ Testes de Error ==============================
 
 
 @pytest.mark.asyncio()
@@ -69,9 +133,6 @@ async def test_updates_existing_email_should_return_409_conflict_error(
     assert response.status_code == status.HTTP_409_CONFLICT
     assert 'detail' in response.json()
     assert response.json()['detail'] == ErrorMessages.EMAIL_ALREADY_EXISTS_409  # type: ignore
-
-
-# @ ======================================================================
 
 
 @pytest.mark.asyncio()
